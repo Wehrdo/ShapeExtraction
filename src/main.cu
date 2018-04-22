89b917d5d79a4bce17839ef24040e6e79e8577a3
David Wehr <dawehr@iastate.edu>
2018-04-22 14:38:36 -0500

    Optimization in CMakeLists

--- src/main.cu
+++ src/main.cu
@@ -13,12 +13,17 @@ using std::endl;
 int main() {
     auto input_cloud = DataIO::loadKitti("../../data/kitti/2011_09_26/2011_09_26_drive_0002_sync/velodyne_points/data/0000000000.bin");
     // auto input_cloud = DataIO::loadObj("../../data/test_sphere_small.obj");
+    std::cout << "Input data has " << input_cloud.x_vals.size() << " points" << std::endl;
 
     auto start_time = std::chrono::high_resolution_clock::now();
     RT::RadixTree radix_tree(input_cloud);
-    OT::Octree octree(radix_tree);
     auto end_time = std::chrono::high_resolution_clock::now();
-    std::cout << "For " << input_cloud.x_vals.size() << " points, construction took " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms" << std::endl;
+    std::cout << "Radix tree construction took " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms" << std::endl;
+
+    start_time = std::chrono::high_resolution_clock::now();
+    OT::Octree octree(radix_tree);
+    end_time = std::chrono::high_resolution_clock::now();
+    std::cout << "Octree construction took " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms" << std::endl;
 
     std::vector<Point> search_pts({
         // Point(50.7, 0.5, 1.9),
@@ -38,20 +43,14 @@ int main() {
     //     std::cout << std::endl;
     // }
 
+    start_time = std::chrono::high_resolution_clock::now();
     auto normals = NormalEstimation::estimateNormals<8>(octree);
-    // for (const auto& pt : normals) {
-    //     std::cout << pt.x << ", " << pt.y << ", " << pt.z << std::endl;
-    // }
+    end_time = std::chrono::high_resolution_clock::now();
+    std::cout << "Normal estimation took " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms" << std::endl;
 
     
     PointCloud<float> output_cloud(octree.h_points, normals);
     output_cloud.saveAsPly("cloud.ply");
 
-    // cloud.saveAsPly("kitti0.ply");
-
-    // size_t n_pts = cloud.x_vals.size();
-    // for (size_t i = 0; i < 50 && i < n_pts; ++i) {
-    //     cout << cloud.x_vals[i] << ", " << cloud.y_vals[i] << ", " << cloud.z_vals[i] << endl;
-    // }
     return 0;
 }
