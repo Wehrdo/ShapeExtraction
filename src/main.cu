6d38167b44b9734cf47335fad3d4db82f60328f1
David Wehr <dawehr@iastate.edu>
2018-04-23 21:25:50 -0500

    more logging

--- src/main.cu
+++ src/main.cu
@@ -26,6 +26,7 @@ void bcastOctree(const int rank, OT::Octree& octree) {
     auto h_nodes = std::make_shared<std::vector<OT::OTNode>>(n_nodes);
     // copy nodes from device to host
     if (rank == 0) {
+        std::cout << "Octree has " << n_nodes << " nodes" << std::endl;
         CudaCheckCall(cudaMemcpy(&(*h_nodes)[0], octree.u_nodes, n_nodes * sizeof(*octree.u_nodes), cudaMemcpyDeviceToHost));
         h_points = octree.h_points;
     }
@@ -60,9 +61,9 @@ int main(int argc, char* argv[]) {
     // Only one node needs to compute the octree
     if (rank == 0) {
         // auto input_cloud = DataIO::loadKitti("../../data/kitti/2011_09_26/2011_09_26_drive_0002_sync/velodyne_points/data/0000000000.bin", 1000000);
-        auto input_cloud = DataIO::loadObj("../../data/test_sphere.obj");
+        // auto input_cloud = DataIO::loadObj("../../data/test_sphere.obj");
         // auto input_cloud = DataIO::loadSemantic3D("../../data/semantic3d/stgallencathedral_station1_intensity_rgb.txt");
-        // auto input_cloud = DataIO::loadKitti("../../data/semantic3d/cathedral1_kitti.bin", 124719076);
+        auto input_cloud = DataIO::loadKitti("../../data/semantic3d/cathedral1_kitti.bin", 124719076);
         std::cout << "Input data has " << input_cloud.x_vals.size() << " points" << std::endl;
         // DataIO::saveKitti(input_cloud, "../../data/semantic3d/cathedral1_kitti.bin");
         // auto reloaded = DataIO::loadKitti("../../data/semantic3d/cathedral1_kitti.bin");
@@ -80,10 +81,13 @@ int main(int argc, char* argv[]) {
         std::cout << "Octree construction took " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms" << std::endl;
     }
 
+    auto start_time = std::chrono::high_resolution_clock::now();
     // Share constructed octree
     bcastOctree(rank, octree);
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::cout << "Took " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms to send octree" << std::endl;
 
-    auto start_time = std::chrono::high_resolution_clock::now();
+    start_time = std::chrono::high_resolution_clock::now();
     int total_pts = octree.n_pts;
     std::vector<int> send_counts(n_nodes);
     std::vector<int> displacements(n_nodes);
@@ -94,9 +98,8 @@ int main(int argc, char* argv[]) {
         send_counts[i] = total_pts / n_nodes + (rank < n_extra);
         cnt_so_far += send_counts[i];
     }
-    std::cout << "Node " << rank << " start = " << displacements[rank] << ", n = " << send_counts[rank] << std::endl;
     auto local_normals = NormalEstimation::estimateNormals<8>(octree, displacements[rank], send_counts[rank]);
-    auto end_time = std::chrono::high_resolution_clock::now();
+    end_time = std::chrono::high_resolution_clock::now();
 
     std::vector<Point> all_normals(rank == 0 ? total_pts : 0);
     MPI_Gatherv(&local_normals[0], send_counts[rank], Point::getMpiDatatype(), rank == 0 ? &all_normals[0] : nullptr, &send_counts[0], &displacements[0], Point::getMpiDatatype(), 0, MPI_COMM_WORLD);
